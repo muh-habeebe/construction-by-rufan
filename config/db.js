742de3cc70da32e55bc2d8472e7a3f0f6e0d7c4e
Muhammed Habeeb Rahman KT <muhhabeeb787@gmail.com>
2026-03-27 01:30:07 +0530

    env copy added

--- config/db.js
+++ config/db.js
@@ -1,13 +1,5 @@
 const mysql = require('mysql2');
-<<<<<<< HEAD
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root', // change as needed
-  database: 'construction_db',
-  connectionLimit: 10
-=======
 require('dotenv').config();
 
 
@@ -22,7 +14,7 @@ const pool = mysql.createPool({
   password: process.env.password,
   database: process.env.database,
   connectionLimit: process.env.connectionLimit
->>>>>>> master
+
 });
 
 module.exports = pool.promise();
